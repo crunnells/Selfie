d9b639fab034e546a2ae31e4918552629182fed2
Chris Runnells <chris@midpacificdesign.com>
2017-05-19 16:18:24 -1000

    Reformat to clear WP.com SVN checks
    When committing to SVN, the WP.com checks squawked about missing <?php
    at the beginning of the file, so reformatted the file to pass the checks

--- components/header/site-branding.php
+++ components/header/site-branding.php
@@ -1,19 +1,19 @@
-	<?php if ( get_header_image() ) : ?>
-		<div class="site-branding" style="background-image: url(<?php header_image(); ?>);">
-	<?php else : ?>
-		<div class="site-branding">
-	<?php endif; // End header image check. ?>
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+<?php if ( get_header_image() ) : ?>
+	<div class="site-branding" style="background-image: url(<?php header_image(); ?>);">
+<?php else : ?>
+	<div class="site-branding">
+<?php endif; // End header image check. ?>
+		<?php
+		if ( is_front_page() && is_home() ) : ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php else : ?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+		<?php
+		endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+		$description = get_bloginfo( 'description', 'display' );
+		if ( $description || is_customize_preview() ) : ?>
+			<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+		<?php
+		endif; ?>
+	</div><!-- .site-branding -->
