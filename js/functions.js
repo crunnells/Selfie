ec50860166b278d9fa25d1ccf6bcea1b27140a88
Chris Runnells <chris@midpacificdesign.com>
2017-05-22 15:14:25 -1000

    Changed .size-big image behavior
     Initially .entry-content and .page-content were full-width at 1000px
    and larger, and child elements were given a fixed width via
    .entry-content > * which worked for the most part but introduced a few
    strange formatting issues where content didn't have a container tag.
    This approach felt hacky anyway, so I fixed the styles so that
    .entry-content was fixed at 660px and .size-big images have a negative
    margin at 1000px and larger.

--- js/functions.js
+++ js/functions.js
@@ -9,6 +9,7 @@
 
 	// After page loads
 	$window.load( function() {
+		galleryWrapper();
 		outdentImages();
 	} );
 
@@ -19,6 +20,7 @@
 
 	// After infinite scroll loads
 	$window.on( 'post-load', function() {
+		galleryWrapper();
 		outdentImages();
 	} );
 
@@ -35,7 +37,6 @@
 			new_img.onload = function(){
 				var img_width = new_img.width;
 
-
 				if ( img_width >= 1000 || img_width >= window.screen.width ) {
 					img.addClass( 'size-big' );
 					img.parents( 'p' ).addClass( 'size-big-wrapper' );
@@ -48,7 +49,7 @@
 					img.parents( 'p' ).removeClass( 'size-big-wrapper' );
 				}
 
-				// On sites with Photon activated, Jetpack is switching the image sizes based on the content width.
+				// On self-hosted sites with Photon activated, Jetpack is switching the image sizes based on the content width.
 				// This leads to instances where the original image is large (eg: > 1000px) but Jetpack is displaying
 				// a smaller one to fit the content area (660px). The original image size triggers the .size-big class
 				// and up-scales the smaller image to 1000px. I'm not really sure how to fix this particular issue right now.
@@ -61,9 +62,10 @@
 					smallest_size = img.width();
 				}
 
-				// on non-Photon sites, this prevents large images with small hard-coded widths from getting .caption-big
+				// on non-Photon sites, this prevents large images or captions with small hard-coded widths
+				// from getting classed '.caption-big'
 				if ( caption.hasClass( 'wp-caption' ) ){
-					if ( smallest_size < 1000 ) {
+					if ( smallest_size < 1000 || caption.width() < 660 ) {
 						img.removeClass( 'size-big' );
 					} else if ( smallest_size >= 1000 ) {
 						img.removeClass( 'size-big' );
@@ -75,6 +77,19 @@
 			// attach the src after the onload is set
 			new_img.src = img.attr('src');
 
+		} );
+	}
+
+	/*
+	 * Wrap tiled gallery
+	 */
+	function galleryWrapper() {
+		$( '.entry-content' ).find( '.tiled-gallery' ).each( function() {
+			if ( ! $( this ).hasClass( 'gallery-wrapped' ) ) {
+				$( this ).wrap( '<div class="tiled-gallery-wrapper"></div>' );
+				$( this ).addClass( 'gallery-wrapped' );
+				$( this ).resize();
+			}
 		} );
 	}
 
